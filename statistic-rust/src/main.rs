ab2c0425f585c603f989f8eb055a783f61cf5ef8
Benito J Palacios <benitojprog@gmail.com>
2023-12-05 13:27:44 +0100

    Update the notebook Data_handling_and_preprocessing

--- statistic-rust/src/main.rs
+++ statistic-rust/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use reqwest::blocking::get;
+use std::io::Read;
+use csv::ReadBuilder;
+use std::error::Error;
+
+#[derive(Debug,Deserialize)]
+struct SalaryRecord {
+    work_year: i32,
+    experience_level: String,
+    employment_type: String,
+    job_title: String,
+    salary: f64,
+    salary_currency: String,
+    salaryusd: f64,
+    employee_residence: String,
+    remote_ratio: f64,
+    company_location: String,
+    company_size: i32,
 }
+
+fn fetch_datase( url: &str ) -> <<ResultBoxstd
